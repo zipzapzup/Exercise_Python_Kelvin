18a5fcf863da67c7c431d28f5063eb7f49e719cd
Kelvin <kelvin.jumino@gmail.com>
2019-06-20 00:01:10 +1000

    Update function_new.py

--- function_new.py
+++ function_new.py
@@ -9,6 +9,28 @@ def AreaSphere(r):
     v =  4.0 / 3.0 * math.pi * r **3
     return v
 
+# Create a function to calculate the area of a circle
+def AreaCircle(r):
+    A = math.pi * r ** 2
+    return A
+
+# Create function to calculate the area of a square
+def AreaSquare(w, l):
+    # w stands for width and l stands for length
+    A = w * l
+    return A
+
+# Create a function to calculate the area of a Triangle
+def AreaTriangle(b, h):
+    # b stands for base and h stands for height
+    A = b * h / 2.0
+    return A
+
+AreaSphere(10)
+AreaCircle(10)
+AreaSquare(10,2)
+AreaTriangle(5,7)
+
 # Creating a function to call which number is higher
 def highernumber(a, b):
     print('Hello there, this function will differentiate between a and b')
