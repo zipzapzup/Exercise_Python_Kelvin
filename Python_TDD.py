d1ad17fafb7e4d11f22281f3cad332294e7db1f8
zipzapzup <kelvin.jumino@gmail.com>
2019-10-23 23:52:52 +1100

    Python Start of TDD

--- Python_TDD.py
+++ Python_TDD.py
@@ -2,6 +2,12 @@
 # Overview
 # Its an approach in Extreme Programming concepts, where the approach is to test the code, prior to deployment into production
 
+# TDD vs UnitTest
+# TDD differs a little compared to Unittest, the definition are:
+# UnitTest: refers to the testing of individual code, to check the logic of a units of behaviour.
+# TDD - test driven development: refers to the approach that testing drives the development. In TDD, there are usually 3 types of test: unit test, functional test and acceptance test.
+
+
 import unittest
 
 
