fd3af5845181ec7672de51b9c6db0b531d1fa61b
zipzapzup <kelvin.jumino@gmail.com>
2019-10-23 23:33:25 +1100

    Python TDD

---
+++ Python_TDD.py
@@ -0,0 +1,8 @@
+# Test Driven Development
+# Overview
+# Its an approach in Extreme Programming concepts, where the approach is to test the code, prior to deployment into production
+
+import unittest
+
+
+print("Hello World - Let's Begin TDD Approach")
