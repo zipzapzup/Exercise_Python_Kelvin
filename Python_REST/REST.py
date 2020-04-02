53558a40c14af6ecc192c25d012bc3dfff82891b
zipzapzup <kelvin.jumino@gmail.com>
2020-04-02 17:16:54 +1100

    REST API data conversion - play around

--- Python_REST/REST.py
+++ Python_REST/REST.py
@@ -3,7 +3,9 @@ import requests
 # Documentation: https://docs.python.org/3/library/urllib.request.html
 # Requests Bin - Inspect HTTP Requests
 # Reqres.in - Fake API data
-# HTTPBin - Fake API
+# HTTPBin - REST server
 
-r = requests.get('https://google.com')
+r = requests.get('https://reqres.in/api/users/2')
 print(r.status_code)
+print(r.text)
+print(r.json()['data']['email'])
