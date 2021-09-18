d96ccbfb88029c27bbe44f1e753d8e021ae6b1a0
zipzapzup <kelvin.jumino@gmail.com>
2021-09-19 02:23:24 +1000

    euclid algo

---
+++ Algorithmn/helloworld.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+// Euclid algorithmn in golang
+// An algorithmn to find the largest common denominator in 2 integers
+
+func algo(a int, b int) int {
+	for b != 0 {
+		remainder := a % b
+		a = b
+		b = remainder
+
+	}
+	return a
+
+}
+
+func main() {
+
+	fmt.Println("hello world")
+	fmt.Println(algo(20, 8))
+	fmt.Println(algo(270, 192))
+}
