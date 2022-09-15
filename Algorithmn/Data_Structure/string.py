3b8c9821f9df93e1c24face0da77a6505426a5c4
zipzapzup <kelvin.jumino@gmail.com>
2022-09-15 15:10:59 +1000

    Algorithmn and More

---
+++ Algorithmn/Data_Structure/string.py
@@ -0,0 +1,12 @@
+# String in Python
+import logging
+
+string1 = "Mary had a little Lamb"
+
+
+def hello():
+    return "Hello World I am here"
+
+logging.warning("Start")
+hello()
+logging.warning("Completed")
