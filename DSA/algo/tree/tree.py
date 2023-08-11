dda850626f35f0230950c95c043cc113b8e5c169
Kelvin <kelvin.jumino@gmail.com>
2023-08-11 15:26:59 +1000

    trees

--- DSA/algo/tree/tree.py
+++ DSA/algo/tree/tree.py
@@ -1,1 +1,64 @@
-# tree
+# Tree
+
+# normal tree implemenetation
+# tree can have more than 1 child
+# represented using a list
+# inefficient tree structure
+# O(n) operation for traverse
+# O(1) operation for addChild *armortised
+class TreeNode:
+    def __init__(self, value):
+        self.data = value
+        self.children = []
+
+    def addChild(self, element):
+        newTree = TreeNode(element)
+        self.children.append(newTree)
+
+    def removeChild(self, target):
+        if target in self.children:
+            self.children.remove(target)
+        else:
+            raise Exception("Child not found")
+        
+    def traverse(self):
+        allnode = [self]
+        while len(allnode) > 0:
+            currentTree = allnode.pop()
+            if currentTree.data is None:
+                break
+            else:
+                print(currentTree.data)
+            allnode = allnode + currentTree.children
+        
+    def countNodes(self):
+        nodes = [self]
+        sum = 0
+        while len(nodes) > 0:
+            current = nodes.pop()
+            sum += 1
+            nodes = nodes + current.children
+        return sum
+
+
+t1 = TreeNode(5)
+t1.addChild(10)
+t1.addChild(7)
+t1.addChild(15)
+t1.addChild(20)
+t1.addChild(25)
+t1.children[0].addChild(99)
+t1.children[0].addChild(20)
+t1.children[1].addChild(100)
+
+t1.traverse()
+
+print("Number of Nodes: ",t1.countNodes())
+
+class BinaryTree:
+    def __init__(self, data, left=None, right =None):
+        self.data = data
+        self.left = left
+        self.right = right
+
+    
